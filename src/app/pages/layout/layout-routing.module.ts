e5d34445a1ccfda1cff9839c8ec7eb1348711ef7
mserranom98 <marco.lis@hotmail.com>
2019-12-15 20:07:14 -0300

    Cambios arreglos

--- src/app/pages/layout/layout-routing.module.ts
+++ src/app/pages/layout/layout-routing.module.ts
@@ -4,6 +4,6 @@ import { Routes } from '@angular/router';
 export const LayoutRoutes: Routes = [
   { path: 'inicio', loadChildren: './pages/content/inicio/inicio.module#InicioModule' },
   { path: 'eventos', loadChildren: './pages/content/eventos/eventos.module#EventosModule' },
-  { path: 'eventos', loadChildren: './pages/content/compras/compras.module#ComprasModule' },
-  { path: 'eventos', loadChildren: './pages/content/localizacion/localizacion.module#LocalizacionModule' }
+  { path: 'compras', loadChildren: './pages/content/compras/compras.module#ComprasModule' },
+  { path: 'localizacion', loadChildren: './pages/content/localizacion/localizacion.module#LocalizacionModule' }
 ];
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule} from '@angular/router';
-import {SidebarComponent} from '../pages/layout/main/sidebar/sidebar.component';
 import {NavbarComponent} from '../pages/layout/main/navbar/navbar.component';
 import {FooterComponent} from '../pages/layout/main/footer/footer.component';
 
@@ -9,12 +8,10 @@ import {FooterComponent} from '../pages/layout/main/footer/footer.component';
 
 @NgModule({
   declarations: [
-    SidebarComponent,
     NavbarComponent,
     FooterComponent
   ],
   exports: [
-    SidebarComponent,
     NavbarComponent,
     FooterComponent
   ],
